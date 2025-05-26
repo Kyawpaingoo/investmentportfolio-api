80b04276e0188a5bce7348a658c9b34663f0c1b6
Kyaw Paing Oo <82670447+Kyawpaingoo@users.noreply.github.com>
2025-05-26 20:04:40 +0700

    ulitity and helper classes added

---
+++ Data/InvestmentPortfolioDBContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Data;
+
+public class InvestmentPortfolioDBContext : DbContext
+{
+    public InvestmentPortfolioDBContext(DbContextOptions<InvestmentPortfolioDBContext> options) : base(options)
+    {
+        
+    }
+    
+    
+}
---
+++ Infra/Helper/Router.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Extensions.Configuration;
+
+namespace Infra;
+
+public struct ConnectionString
+{
+    public static string GetConnectionString()
+    {
+        return 
+            "Host=ep-proud-water-a5jia66v-pooler.us-east-2.aws.neon.tech;Database=neondb;Username=neondb_owner;Password=1TULrKAgqwz6";
+    }
+}
